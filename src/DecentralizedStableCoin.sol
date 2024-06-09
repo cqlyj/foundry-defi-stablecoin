0bcba27dd25bdfa7d9b36cf9286197b59a6b95a9
cqlyj <luoyingjie0721@gmail.com>
2024-06-09 20:42:23 +0800

    Start DecentralizedStableCoin implementation

---
+++ src/DecentralizedStableCoin.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+/**
+ * @title DecentralizedStableCoin
+ * @author Luo Yingjie
+ * Collateral: Exogenous (ETH & BTC)
+ * Minting: Algorithmic
+ * Relative Stability: Pegged to USD
+ *
+ * @notice This is the contract meant to be governed by DSCEngine. This contract is just the ERC20 implementation of the stablecoin system.
+ */
+contract DecentralizedStableCoin {
+    constructor() {}
+}
