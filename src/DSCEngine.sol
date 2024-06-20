3a5794d1570bbc61b8b05dcaf3cb64861f48e0ee
cqlyj <luoyingjie0721@gmail.com>
2024-06-20 23:43:28 +0800

    feat: Add getCollateralTokenPriceFeed function to DSCEngine

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -391,4 +391,10 @@ contract DSCEngine is ReentrancyGuard {
     ) external view returns (uint256) {
         return s_collateralDeposited[user][token];
     }
+
+    function getCollateralTokenPriceFeed(
+        address token
+    ) external view returns (address) {
+        return s_priceFeeds[token];
+    }
 }
